399d6aad05e0debe78a774d3aaca038262709824
sandeepPangare <pangaresandeep96@gmail.com>
2022-09-13 22:26:06 +0530

    Creating a simple Linked list

---
+++ src/com/bridgelabz/Runner.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class Runner {
+    public static void main(String[] args) {
+        LinkedList<Integer> integerLinkedList = new LinkedList<>();
+        integerLinkedList.addAtStart(56);
+        integerLinkedList.print();
+        integerLinkedList.addAtStart(30);
+        integerLinkedList.print();
+        integerLinkedList.addAtStart(70);
+        integerLinkedList.print();
+    }
+}
