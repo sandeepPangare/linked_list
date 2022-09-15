e113c367aeef0e2567c96b08ec2ca69c075251e4
sandeepPangare <pangaresandeep96@gmail.com>
2022-09-15 17:31:18 +0530

    Added:Ability_to_create_Linked_List_by_appending_30_and_70_to_56

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -6,7 +6,7 @@ public class LinkedList<T> {
     public void add(T data) {
         Node<T> newNode = new Node<>();
         newNode.data = data;
-        if(head == null) {
+        if (head == null) {
             head = newNode;
         } else {
             Node<T> node = head;
@@ -23,36 +23,12 @@ public class LinkedList<T> {
         newNode.data = data;
         newNode.next = head;
         head = newNode;
-
-    }
-    public void addAt (int index, T data) {
-        Node<T> newNode = new Node<>();
-        newNode.data = data;
-
-        if(index == 0) {
-            addAtStart(data);
-        } else {
-            Node<T> node = head;
-            for (int i = 0; i < index - 1; i++) {
-                node = node.next;
-            }
-            newNode.next = node.next;
-            node.next = newNode;
-        }
     }
 
     public void print() {
         Node<T> node = head;
         while (node.next != null) {
             System.out.print(node.data + " -> ");
-            node = node.next;
-        }
-        System.out.println(node.data);
-    }
-
-    public void print(int index) {
-        Node<T> node = head;
-        for (int i = 0; i < index - 1; i++) {
             node = node.next;
         }
         System.out.println(node.data);
--- src/com/bridgelabz/Runner.java
+++ src/com/bridgelabz/Runner.java
@@ -3,11 +3,11 @@ package com.bridgelabz;
 public class Runner {
     public static void main(String[] args) {
         LinkedList<Integer> integerLinkedList = new LinkedList<>();
-        integerLinkedList.addAt(0,56);
+        integerLinkedList.add(56);
         integerLinkedList.print();
-        integerLinkedList.addAt(1,30);
+        integerLinkedList.add(30);
         integerLinkedList.print();
-        integerLinkedList.addAt(2,70);
+        integerLinkedList.add(70);
         integerLinkedList.print();
     }
 }
